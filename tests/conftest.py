5e7a7e674d52a2724a15645de8181fc9f1654426
jyablonski9 <jyablonski9@gmail.com>
2021-12-08 16:25:41 -0600

    updating unit testing structure

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,278 +4,119 @@ import pytest_mock
 import sqlite3
 import pandas as pd
 import numpy as np
+import pickle
 from datetime import datetime, timedelta
+from utils import *
 
-@pytest.fixture
+## Testing transformation functions from utils with custom csv fixtures featuring edge cases
+
+@pytest.fixture(scope="session")
 def setup_database():
     """ Fixture to set up an empty in-memory database """
-    conn = sqlite3.connect(':memory:')
+    conn = sqlite3.connect(":memory:")
     yield conn
 
-@pytest.fixture(scope='session')
+
+@pytest.fixture(scope="session")
 def player_stats_data():
     """
     Fixture to load player stats data from a csv file for testing.
     """
-    fname = os.path.join(os.path.dirname(__file__), 'fixture_csvs/player_stats_data.csv')
-    stats = pd.read_csv(fname)
-    stats["PTS"] = pd.to_numeric(stats["PTS"])
-
-    stats = stats.query("Player == Player").reset_index()
-    stats["Player"] = (
-        stats["Player"]
-        .str.normalize("NFKD")
-        .str.encode("ascii", errors="ignore")
-        .str.decode("utf-8")
+    fname = os.path.join(
+        os.path.dirname(__file__), "fixture_csvs/player_stats_data.csv"
     )
-    stats.columns = stats.columns.str.lower()
-    stats["scrape_date"] = datetime.now().date()
-    stats = stats.drop("index", axis=1)
+    stats = pd.read_csv(fname)
+    stats = get_player_stats_transformed(stats)
     return stats
 
-@pytest.fixture(scope='session')
+
+@pytest.fixture(scope="session")
 def boxscores_data():
     """
     Fixture to load boxscores data from a csv file for testing.
     """
-    fname = os.path.join(os.path.dirname(__file__), 'fixture_csvs/boxscores_data.csv')
+    fname = os.path.join(os.path.dirname(__file__), "fixture_csvs/boxscores_data.csv")
     df = pd.read_csv(fname)
     day = (datetime.now() - timedelta(1)).day
     month = (datetime.now() - timedelta(1)).month
     year = (datetime.now() - timedelta(1)).year
-    season_type = 'Regular Season'
-    df[
-            [
-                "FGM",
-                "FGA",
-                "FGPercent",
-                "threePFGMade",
-                "threePAttempted",
-                "threePointPercent",
-                "OREB",
-                "DREB",
-                "TRB",
-                "AST",
-                "STL",
-                "BLK",
-                "TOV",
-                "PF",
-                "PTS",
-                "PlusMinus",
-                "GmSc",
-            ]
-        ] = df[
-            [
-                "FGM",
-                "FGA",
-                "FGPercent",
-                "threePFGMade",
-                "threePAttempted",
-                "threePointPercent",
-                "OREB",
-                "DREB",
-                "TRB",
-                "AST",
-                "STL",
-                "BLK",
-                "TOV",
-                "PF",
-                "PTS",
-                "PlusMinus",
-                "GmSc",
-            ]
-        ].apply(
-            pd.to_numeric
-        )
-    df["date"] = str(year) + "-" + str(month) + "-" + str(day)
-    df["date"] = pd.to_datetime(df["date"])
-    df["Type"] = season_type
-    df["Season"] = 2022
-    df["Location"] = df["Location"].apply(lambda x: "A" if x == "@" else "H")
-    df["Team"] = df["Team"].str.replace("PHO", "PHX")
-    df["Team"] = df["Team"].str.replace("CHO", "CHA")
-    df["Team"] = df["Team"].str.replace("BRK", "BKN")
-    df["Opponent"] = df["Opponent"].str.replace("PHO", "PHX")
-    df["Opponent"] = df["Opponent"].str.replace("CHO", "CHA")
-    df["Opponent"] = df["Opponent"].str.replace("BRK", "BKN")
-    df = df.query("Player == Player").reset_index(drop=True)
-    df["Player"] = (
-        df["Player"]
-        .str.normalize("NFKD")
-        .str.encode("ascii", errors="ignore")
-        .str.decode("utf-8")
-    )
-    df.columns = df.columns.str.lower()
+    season_type = "Regular Season"
+    df = get_boxscores_transformed(df)
     return df
 
-@pytest.fixture(scope='session')
+
+@pytest.fixture(scope="session")
 def opp_stats_data():
     """
     Fixture to load team opponent stats data from a csv file for testing.
     """
-    fname = os.path.join(os.path.dirname(__file__), 'fixture_csvs/opp_stats_data.csv')
+    fname = os.path.join(os.path.dirname(__file__), "fixture_csvs/opp_stats_data.csv")
     df = pd.read_csv(fname)
-    df = df[["Team", "FG%", "3P%", "3P", "PTS"]]
-    df = df.rename(
-        columns={
-            df.columns[0]: "team",
-            df.columns[1]: "fg_percent_opp",
-            df.columns[2]: "threep_percent_opp",
-            df.columns[3]: "threep_made_opp",
-            df.columns[4]: "ppg_opp",
-        }
-    )
-    df = df.query('team != "League Average"')
-    df = df.reset_index(drop=True)
-    df["scrape_date"] = datetime.now().date()
+    df = get_opp_stats_transformed(df)
     return df
 
-@pytest.fixture(scope='session')
+
+@pytest.fixture(scope="session")
 def injuries_data():
     """
     Fixture to load injuries data from a csv file for testing.
     """
-    fname = os.path.join(os.path.dirname(__file__), 'fixture_csvs/injuries_data.csv')
+    fname = os.path.join(os.path.dirname(__file__), "fixture_csvs/injuries_data.csv")
     df = pd.read_csv(fname)
-    df = df.rename(columns={"Update": "Date"})
-    df.columns = df.columns.str.lower()
-    df["scrape_date"] = datetime.now().date()
+    df = get_injuries_transformed(df)
     return df
 
-@pytest.fixture(scope='session')
+
+@pytest.fixture(scope="session")
 def transactions_data():
     """
     Fixture to load transactions data from a csv file for testing.
     """
-    fname = os.path.join(os.path.dirname(__file__), 'fixture_csvs/transactions_data.csv')
-    transactions = pd.read_csv(fname)
-    transactions.columns = ["Date", "Transaction"]
-    transactions = transactions.query(
-        'Date == Date & Date != ""'
-    ).reset_index()  # filters out nulls and empty values
-    transactions = transactions.explode("Transaction")
-    transactions["Date"] = transactions["Date"].str.replace(
-        "?", "Jan 1, 2021", regex=True  # bad data 10-14-21
+    fname = os.path.join(
+        os.path.dirname(__file__), "fixture_csvs/transactions_data.csv"
     )
-    transactions["Date"] = pd.to_datetime(transactions["Date"])
-    transactions.columns = transactions.columns.str.lower()
-    transactions = transactions[["date", "transaction"]]
-    transactions["scrape_date"] = datetime.now().date()
+    transactions = pd.read_csv(fname)
+    transactions = get_transactions_transformed(transactions)
     return transactions
 
-@pytest.fixture(scope='session')
+
+@pytest.fixture(scope="session")
 def advanced_stats_data():
     """
     Fixture to load team advanced stats data from a csv file for testing.
     """
-    fname = os.path.join(os.path.dirname(__file__), 'fixture_csvs/advanced_stats_data.csv')
+    fname = os.path.join(
+        os.path.dirname(__file__), "fixture_csvs/advanced_stats_data.csv"
+    )
     df = pd.read_csv(fname)
-    df.drop(columns=df.columns[0], axis=1, inplace=True)
-
-    df.columns = [
-        "Team",
-        "Age",
-        "W",
-        "L",
-        "PW",
-        "PL",
-        "MOV",
-        "SOS",
-        "SRS",
-        "ORTG",
-        "DRTG",
-        "NRTG",
-        "Pace",
-        "FTr",
-        "3PAr",
-        "TS%",
-        "bby1",  # the bby columns are because of hierarchical html formatting - they're just blank columns
-        "eFG%",
-        "TOV%",
-        "ORB%",
-        "FT/FGA",
-        "bby2",
-        "eFG%_opp",
-        "TOV%_opp",
-        "DRB%_opp",
-        "FT/FGA_opp",
-        "bby3",
-        "Arena",
-        "Attendance",
-        "Att/Game",
-    ]
-    df.drop(["bby1", "bby2", "bby3"], axis=1, inplace=True)
-    df = df.query('Team != "League Average"').reset_index()
-    df["Team"] = df["Team"].str.replace("*", "", regex=True)
-    df["scrape_date"] = datetime.now().date()
-    df.columns = df.columns.str.lower()
+    df = get_advanced_stats_transformed(df)
     return df
 
-@pytest.fixture(scope='session')
+
+# has to be pickle bc odds data can be returned in list of 1 or 2 objects
+@pytest.fixture(scope="session")
 def odds_data():
     """
     Fixture to load odds data from a csv file for testing.
     """
-    fname = os.path.join(os.path.dirname(__file__), 'fixture_csvs/odds_data.csv')
-    df = pd.read_csv(fname)
+    fname = os.path.join(os.path.dirname(__file__), "fixture_csvs/odds_data")
+    with open(fname, "rb") as fp:
+        df = pickle.load(fp)
     day = (datetime.now() - timedelta(1)).day
     month = (datetime.now() - timedelta(1)).month
     year = (datetime.now() - timedelta(1)).year
-    data1 = df[0].copy()
-    data1.columns.values[0] = "Tomorrow"
-    date_try = str(year) + " " + data1.columns[0]
-    data1["date"] = np.where(
-        date_try == "2021 Tomorrow",
-        datetime.now().date(),  # if the above is true, then return this
-        str(year) + " " + data1.columns[0],  # if false then return this
-    )
-    # )
-    date_try = data1["date"].iloc[0]
-    data1.reset_index(drop=True)
-    data1["Tomorrow"] = data1["Tomorrow"].str.replace(
-        "LA Clippers", "LAC Clippers", regex=True
-    )
+    df = get_odds_transformed(df)
+    return df
 
-    data1["Tomorrow"] = data1["Tomorrow"].str.replace("AM", "AM ", regex=True)
-    data1["Tomorrow"] = data1["Tomorrow"].str.replace("PM", "PM ", regex=True)
-    data1["Time"] = data1["Tomorrow"].str.split().str[0]
-    data1["datetime1"] = (
-        pd.to_datetime(date_try.strftime("%Y-%m-%d") + " " + data1["Time"])
-        - timedelta(hours=6)
-        + timedelta(days=1)
-    )
 
-    data2 = df[1].copy()
-    data2.columns.values[0] = "Tomorrow"
-    data2.reset_index(drop=True)
-    data2["Tomorrow"] = data2["Tomorrow"].str.replace(
-        "LA Clippers", "LAC Clippers", regex=True
+@pytest.fixture(scope="session")
+def player_transformed_stats_data():
+    """
+    Fixture to load player stats data from a csv file for testing.
+    """
+    fname = os.path.join(
+        os.path.dirname(__file__), "fixture_csvs/player_stats_data.csv"
     )
-    data2["Tomorrow"] = data2["Tomorrow"].str.replace("AM", "AM ", regex=True)
-    data2["Tomorrow"] = data2["Tomorrow"].str.replace("PM", "PM ", regex=True)
-    data2["Time"] = data2["Tomorrow"].str.split().str[0]
-    data2["datetime1"] = (
-        pd.to_datetime(date_try.strftime("%Y-%m-%d") + " " + data2["Time"])
-        - timedelta(hours=6)
-        + timedelta(days=1)
-    )
-    data2["date"] = data2["datetime1"].dt.date
-
-    data = data1.append(data2).reset_index(drop=True)
-    data["SPREAD"] = data["SPREAD"].str[:-4]
-    data["TOTAL"] = data["TOTAL"].str[:-4]
-    data["TOTAL"] = data["TOTAL"].str[2:]
-    data["Tomorrow"] = data["Tomorrow"].str.split().str[1:2]
-    data["Tomorrow"] = pd.DataFrame(
-        [
-            str(line).strip("[").strip("]").replace("'", "")
-            for line in data["Tomorrow"]
-        ]
-    )
-    data["SPREAD"] = data["SPREAD"].str.replace("pk", "-1", regex=True)
-    data["SPREAD"] = data["SPREAD"].str.replace("+", "", regex=True)
-    data.columns = data.columns.str.lower()
-    data = data[["tomorrow", "spread", "total", "moneyline", "date", "datetime1"]]
-    data = data.rename(columns={data.columns[0]: "team"})
-    data = data.query("date == date.min()")  # only grab games from upcoming day
-    return data
+    stats = pd.read_csv(fname)
+    stats_transformed = get_player_stats_transformed(stats)
+    return stats_transformed
--- tests/unit_test.py
+++ tests/unit_test.py
@@ -1,68 +1,87 @@
 import pytest
 
+
 def test_player_stats_sql(setup_database, player_stats_data):
     df = player_stats_data
     print(len(df))
     cursor = setup_database.cursor()
-    df.to_sql(con = setup_database, name = 'player_stats', index = False, if_exists = 'replace')
-    df_len = len(list(cursor.execute('SELECT * FROM player_stats')))
+    df.to_sql(con=setup_database, name="player_stats", index=False, if_exists="replace")
+    df_len = len(list(cursor.execute("SELECT * FROM player_stats")))
     cursor.close()
     assert df_len >= 300
+
 
 def test_boxscores_sql(setup_database, boxscores_data):
     df = boxscores_data
     print(len(df))
     cursor = setup_database.cursor()
-    df.to_sql(con = setup_database, name = 'boxscores', index = False, if_exists = 'replace')
-    df_len = len(list(cursor.execute('SELECT * FROM boxscores')))
+    df.to_sql(con=setup_database, name="boxscores", index=False, if_exists="replace")
+    df_len = len(list(cursor.execute("SELECT * FROM boxscores")))
     cursor.close()
     assert df_len >= 20
+
 
 def test_opp_stats_sql(setup_database, opp_stats_data):
     df = opp_stats_data
     print(len(df))
     cursor = setup_database.cursor()
-    df.to_sql(con = setup_database, name = 'opp_stats', index = False, if_exists = 'replace')
-    df_len = len(list(cursor.execute('SELECT * FROM opp_stats')))
+    df.to_sql(con=setup_database, name="opp_stats", index=False, if_exists="replace")
+    df_len = len(list(cursor.execute("SELECT * FROM opp_stats")))
     cursor.close()
     assert df_len == 30
+
 
 def test_injuries_sql(setup_database, injuries_data):
     df = injuries_data
     print(len(df))
     cursor = setup_database.cursor()
-    df.to_sql(con = setup_database, name = 'injuries', index = False, if_exists = 'replace')
-    df_len = len(list(cursor.execute('SELECT * FROM injuries')))
+    df.to_sql(con=setup_database, name="injuries", index=False, if_exists="replace")
+    df_len = len(list(cursor.execute("SELECT * FROM injuries")))
     cursor.close()
     assert df_len >= 10
+
 
 def test_transactions_sql(setup_database, transactions_data):
     df = transactions_data
     print(len(df))
     cursor = setup_database.cursor()
-    df.to_sql(con = setup_database, name = 'transactions', index = False, if_exists = 'replace')
-    df_len = len(list(cursor.execute('SELECT * FROM transactions')))
+    df.to_sql(con=setup_database, name="transactions", index=False, if_exists="replace")
+    df_len = len(list(cursor.execute("SELECT * FROM transactions")))
     cursor.close()
     assert df_len >= 50
+
 
 def test_advanced_stats_sql(setup_database, advanced_stats_data):
     df = advanced_stats_data
     print(len(df))
     cursor = setup_database.cursor()
-    df.to_sql(con = setup_database, name = 'advanced_stats', index = False, if_exists = 'replace')
-    df_len = len(list(cursor.execute('SELECT * FROM advanced_stats')))
+    df.to_sql(
+        con=setup_database, name="advanced_stats", index=False, if_exists="replace"
+    )
+    df_len = len(list(cursor.execute("SELECT * FROM advanced_stats")))
     cursor.close()
     assert df_len == 31
 
+
 def test_player_stats_rows(player_stats_data):
     assert len(player_stats_data) >= 300
+
 
 def test_player_stats_cols(player_stats_data):
     assert len(player_stats_data.columns) == 30
 
 
+def test_player_transformed_stats_rows(player_transformed_stats_data):
+    assert len(player_transformed_stats_data) >= 300
+
+
+def test_player_transformed_stats_cols(player_transformed_stats_data):
+    assert len(player_transformed_stats_data.columns) == 30
+
+
 def test_boxscores_cols(boxscores_data):
     assert len(boxscores_data.columns) == 29
+
 
 def test_boxscores_rows(boxscores_data):
     assert len(boxscores_data) >= 20
@@ -71,12 +90,22 @@ def test_boxscores_rows(boxscores_data):
 def test_opp_stats_cols(opp_stats_data):
     assert len(opp_stats_data.columns) == 6
 
+
 def test_opp_stats_rows(opp_stats_data):
     assert len(opp_stats_data) == 30
 
 
+def test_odds_cols(odds_data):
+    assert len(odds_data.columns) == 6
+
+
+def test_odds_rows(odds_data):
+    assert len(odds_data) == 26
+
+
 def test_injuries_cols(injuries_data):
     assert len(injuries_data.columns) == 5
+
 
 def test_injuries_rows(injuries_data):
     assert len(injuries_data) >= 10
@@ -85,15 +114,25 @@ def test_injuries_rows(injuries_data):
 def test_transactions_cols(transactions_data):
     assert len(transactions_data.columns) == 3
 
+
 def test_transactions_rows(transactions_data):
     assert len(transactions_data) >= 50
 
 
 def test_advanced_stats_cols(advanced_stats_data):
-    assert len(advanced_stats_data.columns) == 29 ##
+    assert len(advanced_stats_data.columns) == 29  ##
+
 
 def test_advanced_stats_rows(advanced_stats_data):
     assert len(advanced_stats_data) == 31
 
+
 def test_opp_stats_colnames(opp_stats_data):
-    assert list(opp_stats_data.columns) == ['team', 'fg_percent_opp', 'threep_percent_opp', 'threep_made_opp', 'ppg_opp', 'scrape_date']
+    assert list(opp_stats_data.columns) == [
+        "team",
+        "fg_percent_opp",
+        "threep_percent_opp",
+        "threep_made_opp",
+        "ppg_opp",
+        "scrape_date",
+    ]
