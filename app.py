6cdbc3a70931ea9fde2a0670fe9fde290bdb521f
jyablonski9 <jyablonski9@gmail.com>
2021-12-16 12:08:18 -0600

    trying all in __name__ == "__main__"

--- app.py
+++ app.py
@@ -69,60 +69,59 @@ month = (datetime.now() - timedelta(1)).month
 year = (datetime.now() - timedelta(1)).year
 season_type = "Regular Season"
 
-print("STARTING WEB SCRAPE")
-logging.info("STARTING WEB SCRAPE")
-
-# STEP 1: Grab Raw Data
-stats_raw = get_player_stats_data()
-boxscores_raw = get_boxscores_data()
-injury_data_raw = get_injuries_data()
-transactions_raw = get_transactions_data()
-adv_stats_raw = get_advanced_stats_data()
-odds_raw = get_odds_data()
-reddit_data = get_reddit_data("nba")  # doesnt need transformation
-opp_stats_raw = get_opp_stats_data()
-
-print("FINISHED WEB SCRAPE")
-logging.info("FINISHED WEB SCRAPE")
-
-
-# STEP 2: Transform data
-print("STARTING DATA TRANSFORMATIONS")
-logging.info("STARTING DATA TRANSFORMATIONS")
-
-stats = get_player_stats_transformed(stats_raw)
-boxscores = get_boxscores_transformed(boxscores_raw)
-injury_data = get_injuries_transformed(injury_data_raw)
-transactions = get_transactions_transformed(transactions_raw)
-adv_stats = get_advanced_stats_transformed(adv_stats_raw)
-odds = get_odds_transformed(odds_raw)
-pbp_data = get_pbp_data_transformed(boxscores)  # this uses the transformed boxscores
-opp_stats = get_opp_stats_transformed(opp_stats_raw)
-
-print("FINISHED DATA TRANSFORMATIONS")
-logging.info("FINISHED DATA TRANSFORMATIONS")
-
-print("STARTING SQL STORING")
-logging.info("STARTING SQL STORING")
-
-# STEP 3: Append Transformed Data to SQL
-conn = sql_connection(schema="nba_source")
-write_to_sql(conn, stats, "append")
-write_to_sql(conn, boxscores, "append")
-write_to_sql(conn, injury_data, "append")
-write_to_sql(conn, transactions, "append")
-write_to_sql(conn, adv_stats, "append")
-write_to_sql(conn, odds, "append")
-write_to_sql(conn, reddit_data, "append")
-write_to_sql(conn, pbp_data, "append")
-write_to_sql(conn, opp_stats, "append")
-
-# STEP 4: Grab Logs from previous steps & send email out detailing notable events
-logs = pd.read_csv("example.log", sep=r"\\t", engine="python", header=None)
-logs = logs.rename(columns={0: "errors"})
-logs = logs.query("errors.str.contains('Failed')", engine="python")
-
 if __name__ == "__main__":
+    print("STARTING WEB SCRAPE")
+    logging.info("STARTING WEB SCRAPE")
+
+    # STEP 1: Grab Raw Data
+    stats_raw = get_player_stats_data()
+    boxscores_raw = get_boxscores_data()
+    injury_data_raw = get_injuries_data()
+    transactions_raw = get_transactions_data()
+    adv_stats_raw = get_advanced_stats_data()
+    odds_raw = get_odds_data()
+    reddit_data = get_reddit_data("nba")  # doesnt need transformation
+    opp_stats_raw = get_opp_stats_data()
+
+    print("FINISHED WEB SCRAPE")
+    logging.info("FINISHED WEB SCRAPE")
+
+
+    # STEP 2: Transform data
+    print("STARTING DATA TRANSFORMATIONS")
+    logging.info("STARTING DATA TRANSFORMATIONS")
+
+    stats = get_player_stats_transformed(stats_raw)
+    boxscores = get_boxscores_transformed(boxscores_raw)
+    injury_data = get_injuries_transformed(injury_data_raw)
+    transactions = get_transactions_transformed(transactions_raw)
+    adv_stats = get_advanced_stats_transformed(adv_stats_raw)
+    odds = get_odds_transformed(odds_raw)
+    pbp_data = get_pbp_data_transformed(boxscores)  # this uses the transformed boxscores
+    opp_stats = get_opp_stats_transformed(opp_stats_raw)
+
+    print("FINISHED DATA TRANSFORMATIONS")
+    logging.info("FINISHED DATA TRANSFORMATIONS")
+
+    print("STARTING SQL STORING")
+    logging.info("STARTING SQL STORING")
+
+    # STEP 3: Append Transformed Data to SQL
+    conn = sql_connection(schema="nba_source")
+    write_to_sql(conn, stats, "append")
+    write_to_sql(conn, boxscores, "append")
+    write_to_sql(conn, injury_data, "append")
+    write_to_sql(conn, transactions, "append")
+    write_to_sql(conn, adv_stats, "append")
+    write_to_sql(conn, odds, "append")
+    write_to_sql(conn, reddit_data, "append")
+    write_to_sql(conn, pbp_data, "append")
+    write_to_sql(conn, opp_stats, "append")
+
+    # STEP 4: Grab Logs from previous steps & send email out detailing notable events
+    logs = pd.read_csv("example.log", sep=r"\\t", engine="python", header=None)
+    logs = logs.rename(columns={0: "errors"})
+    logs = logs.query("errors.str.contains('Failed')", engine="python")
     execute_email_function(logs)
 
 print("FINISHED NBA ELT PIPELINE SCRIPT Version: 1.0.5")
