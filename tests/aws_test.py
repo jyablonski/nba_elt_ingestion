6bb7028928b187cae941bd09a5578100d551d994
jyablonski9 <jyablonski9@gmail.com>
2022-05-25 17:01:06 -0500

    separate tests

---
+++ tests/aws_test.py
@@ -0,0 +1,77 @@
+from datetime import datetime
+
+import awswrangler as wr
+import boto3
+from moto import mock_ses, mock_s3
+import numpy as np
+import pandas as pd
+import pytest
+
+from src.utils import (
+    write_to_s3,
+    send_aws_email,
+    execute_email_function,
+    get_leading_zeroes,
+)
+
+
+@mock_ses
+def test_ses_email(aws_credentials):
+    ses = boto3.client("ses", region_name="us-east-1")
+    logs = pd.DataFrame({"errors": ["ex1", "ex2", "ex3"]})
+    send_aws_email(logs)
+    assert ses.verify_email_identity(EmailAddress="jyablonski9@gmail.com")
+
+
+@mock_ses
+def test_ses_execution_logs(aws_credentials):
+    ses = boto3.client("ses", region_name="us-east-1")
+    logs = pd.DataFrame({"errors": ["ex1", "ex2", "ex3"]})
+    execute_email_function(logs)
+    assert ses.verify_email_identity(EmailAddress="jyablonski9@gmail.com")
+
+
+@mock_ses
+def test_ses_execution_no_logs(aws_credentials):
+    ses = boto3.client("ses", region_name="us-east-1")
+    logs = pd.DataFrame({"errors": []})
+    send_aws_email(logs)
+    assert ses.verify_email_identity(EmailAddress="jyablonski9@gmail.com")
+
+
+@mock_s3
+def test_write_to_s3_validated(player_stats_data):
+    conn = boto3.resource("s3", region_name="us-east-1")
+    today = datetime.now().date()
+    month = datetime.now().month
+    month_prefix = get_leading_zeroes(month)
+    player_stats_data.schema = "Validated"
+    conn.create_bucket(Bucket="moto_test_bucket")
+
+    write_to_s3("player_stats_data", player_stats_data, bucket="moto_test_bucket")
+    bucket = conn.Bucket("moto_test_bucket")
+    contents = [_.key for _ in bucket.objects.all()]
+
+    assert (
+        contents[0]
+        == f"player_stats_data/validated/{month_prefix}/player_stats_data-{today}.parquet"
+    )
+
+
+@mock_s3
+def test_write_to_s3_invalidated(player_stats_data):
+    conn = boto3.resource("s3", region_name="us-east-1")
+    today = datetime.now().date()
+    month = datetime.now().month
+    month_prefix = get_leading_zeroes(month)
+    player_stats_data.schema = "Invalidated"
+    conn.create_bucket(Bucket="moto_test_bucket")
+
+    write_to_s3("player_stats_data", player_stats_data, bucket="moto_test_bucket")
+    bucket = conn.Bucket("moto_test_bucket")
+    contents = [_.key for _ in bucket.objects.all()]
+
+    assert (
+        contents[0]
+        == f"player_stats_data/invalidated/{month_prefix}/player_stats_data-{today}.parquet"
+    )
---
+++ tests/sql_test.py
@@ -0,0 +1,55 @@
+from datetime import datetime
+
+import awswrangler as wr
+import boto3
+from moto import mock_ses, mock_s3
+import numpy as np
+import pandas as pd
+import pytest
+
+from src.utils import write_to_sql, write_to_sql_upsert
+
+# WRITE TO SQL TESTS
+def test_player_stats_sql(setup_database, player_stats_data):
+    cursor = setup_database.cursor()
+    player_stats_data.to_sql(
+        con=setup_database,
+        name="aws_player_stats_data_source",
+        index=False,
+        if_exists="replace",
+    )
+    df_len = len(list(cursor.execute("SELECT * FROM aws_player_stats_data_source")))
+    cursor.close()
+    assert df_len == 384
+
+
+# table has to get created from ^ first, other wise this will error out.
+def test_write_to_sql(setup_database, player_stats_data):
+    conn = setup_database.cursor()
+    player_stats_data.schema = "Validated"
+    write_to_sql(conn, "player_stats_data", player_stats_data, "append")
+    df_len = len(list(conn.execute("SELECT * FROM aws_player_stats_data_source")))
+    assert df_len == 384
+
+
+def test_write_to_sql_no_data(setup_database):
+    conn = setup_database.cursor()
+    player_stats_data = pd.DataFrame({"errors": []})
+    player_stats_data.schema = "Invalidated"
+    write_to_sql(conn, "player_stats_data", player_stats_data, "append")
+    df_len = len(list(conn.execute("SELECT * FROM aws_player_stats_data_source")))
+    assert len(player_stats_data) == 0
+
+
+# don't think this is possible bc i use postgres which has different syntax and constraint stuff and schemas
+# mocking half of the function out seems stupid right now
+# def test_write_to_sql_upsert(setup_database, schedule_upsert_data_1, schedule_upsert_data_2):
+#     conn = setup_database.cursor()
+#     write_to_sql_upsert(setup_database, "schedule_dummy", schedule_upsert_data_1, "upsert", pd_index = ["away_team", "home_team", "proper_date"], is_test = True)
+#     write_to_sql_upsert(setup_database, "schedule_dummy", schedule_upsert_data_2, "upsert", pd_index = ["away_team", "home_team", "proper_date"], is_test = True)
+#     write_to_sql_upsert(setup_database, "schedule_dummy", schedule_upsert_data_2, "upsert", pd_index = ["away_team", "home_team", "proper_date"], is_test = True)
+#     # intentionally write it twice to make sure data doesn't get duplicated
+
+#     df_len = len(list(conn.execute("SELECT * from schedule_dummy;")))
+#     conn.close()
+#     assert df_len == 6
