2563eb97a4487f2ee481a3b6786d486bda5fb768
jyablonski9 <jyablonski9@gmail.com>
2022-09-25 10:39:56 -0500

    Bump version: 1.6.2 → 1.7.0

--- src/app.py
+++ src/app.py
@@ -29,7 +29,7 @@ logging.basicConfig(
     handlers=[logging.FileHandler("logs/example.log"), logging.StreamHandler()],
 )
 logging.getLogger("requests").setLevel(logging.WARNING)  # get rid of https debug stuff
-logging.info("STARTING NBA ELT PIPELINE SCRIPT Version: 1.6.2")
+logging.info("STARTING NBA ELT PIPELINE SCRIPT Version: 1.7.0")
 
 # helper validation function - has to be here instead of utils bc of globals().items()
 def validate_schema(df: pd.DataFrame, schema: list) -> pd.DataFrame:
@@ -174,4 +174,4 @@ if __name__ == "__main__":
 
     # STEP 6: Send Email
     send_aws_email(logs)
-    logging.info("FINISHED NBA ELT PIPELINE SCRIPT Version: 1.6.2")
+    logging.info("FINISHED NBA ELT PIPELINE SCRIPT Version: 1.7.0")
