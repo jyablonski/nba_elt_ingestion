019858ea917f4f478c7529470234426cc1c3c3ef
jyablonski9 <jyablonski9@gmail.com>
2022-10-18 18:59:11 -0500

    season opener

--- src/app.py
+++ src/app.py
@@ -72,41 +72,39 @@ if __name__ == "__main__":
     logging.info("STARTING WEB SCRAPE")
 
     # STEP 1: Extract Raw Data
-    # stats = get_player_stats_data()
-    # boxscores = get_boxscores_data()
+    stats = get_player_stats_data()
+    boxscores = get_boxscores_data()
     injury_data = get_injuries_data()
     transactions = get_transactions_data()
-    # adv_stats = get_advanced_stats_data()
-    # odds = get_odds_data()
+    adv_stats = get_advanced_stats_data()
+    odds = get_odds_data()
     reddit_data = get_reddit_data("nba")  # doesnt need transformation
-    # opp_stats = get_opp_stats_data()
+    opp_stats = get_opp_stats_data()
     # schedule = schedule_scraper("2022", ["april", "may", "june"])
-    # shooting_stats = get_shooting_stats_data()
+    shooting_stats = get_shooting_stats_data()
     twitter_tweepy_data = scrape_tweets_combo()
     reddit_comment_data = get_reddit_comments(reddit_data["reddit_url"])
-    # pbp_data = get_pbp_data(
-    #     boxscores
-    # )  # this uses the transformed boxscores
+    pbp_data = get_pbp_data(
+        boxscores
+    )  # this uses the transformed boxscores
 
     logging.info("FINISHED WEB SCRAPE")
 
     logging.info("STARTING SCHEMA VALIDATION")
 
     # STEP 2: Validating Schemas - 1 for each SQL Write
-    # stats = validate_schema(stats, stats_cols)
-    # adv_stats = validate_schema(adv_stats, adv_stats_cols)
-    # boxscores = validate_schema(boxscores, boxscores_cols)
+    stats = validate_schema(stats, stats_cols)
+    adv_stats = validate_schema(adv_stats, adv_stats_cols)
+    boxscores = validate_schema(boxscores, boxscores_cols)
     injury_data = validate_schema(injury_data, injury_cols)
-    # opp_stats = validate_schema(opp_stats, opp_stats_cols)
-    # pbp_stats = validate_schema(pbp_data, pbp_cols)
+    opp_stats = validate_schema(opp_stats, opp_stats_cols)
+    pbp_stats = validate_schema(pbp_data, pbp_cols)
     reddit_data = validate_schema(reddit_data, reddit_cols)
     reddit_comment_data = validate_schema(reddit_comment_data, reddit_comment_cols)
-    # odds = validate_schema(odds, odds_cols)
-    transactions = validate_schema(transactions, transactions_cols)
-    # twitter_data = validate_schema(twitter_data, twitter_cols)
+    odds = validate_schema(odds, odds_cols)
     twitter_tweepy_data = validate_schema(twitter_tweepy_data, twitter_tweepy_cols)
     # schedule = validate_schema(schedule, schedule_cols)
-    # shooting_stats = validate_schema(shooting_stats, shooting_stats_cols)
+    shooting_stats = validate_schema(shooting_stats, shooting_stats_cols)
 
     logging.info("FINISHED SCHEMA VALIDATION")
 
@@ -117,31 +115,31 @@ if __name__ == "__main__":
 
     with conn.connect() as connection:
         # new upserts
-        # write_to_sql_upsert(connection, "stats", stats, "upsert", ["player"])
-        # write_to_sql_upsert(
-        #     connection, "boxscores", boxscores, "upsert", ["player", "date"]
-        # )
-        # write_to_sql_upsert(connection, "adv_stats", adv_stats, "upsert", ["team"])
-        # write_to_sql_upsert(connection, "odds", odds, "upsert", ["team", "date"])
-        # write_to_sql_upsert(
-        #     connection,
-        #     "pbp_data",
-        #     pbp_data,
-        #     "upsert",
-        #     [
-        #         "hometeam",
-        #         "awayteam",
-        #         "date",
-        #         "timequarter",
-        #         "numberperiod",
-        #         "descriptionplayvisitor",
-        #         "descriptionplayhome",
-        #     ],
-        # )
-        # write_to_sql_upsert(connection, "opp_stats", opp_stats, "upsert", ["team"])
-        # write_to_sql_upsert(
-        #     connection, "shooting_stats", shooting_stats, "upsert", ["player"]
-        # )
+        write_to_sql_upsert(connection, "stats", stats, "upsert", ["player"])
+        write_to_sql_upsert(
+            connection, "boxscores", boxscores, "upsert", ["player", "date"]
+        )
+        write_to_sql_upsert(connection, "adv_stats", adv_stats, "upsert", ["team"])
+        write_to_sql_upsert(connection, "odds", odds, "upsert", ["team", "date"])
+        write_to_sql_upsert(
+            connection,
+            "pbp_data",
+            pbp_data,
+            "upsert",
+            [
+                "hometeam",
+                "awayteam",
+                "date",
+                "timequarter",
+                "numberperiod",
+                "descriptionplayvisitor",
+                "descriptionplayhome",
+            ],
+        )
+        write_to_sql_upsert(connection, "opp_stats", opp_stats, "upsert", ["team"])
+        write_to_sql_upsert(
+            connection, "shooting_stats", shooting_stats, "upsert", ["player"]
+        )
         write_to_sql_upsert(
             connection, "reddit_data", reddit_data, "upsert", ["reddit_url"]
         )
@@ -175,19 +173,19 @@ if __name__ == "__main__":
     conn.dispose()
 
     # STEP 4: Write to S3
-    # write_to_s3("stats", stats)
-    # write_to_s3("boxscores", boxscores)
+    write_to_s3("stats", stats)
+    write_to_s3("boxscores", boxscores)
     write_to_s3("injury_data", injury_data)
     write_to_s3("transactions", transactions)
-    # write_to_s3("adv_stats", adv_stats)
-    # write_to_s3("odds", odds)
+    write_to_s3("adv_stats", adv_stats)
+    write_to_s3("odds", odds)
     write_to_s3("reddit_data", reddit_data)
     write_to_s3("reddit_comment_data", reddit_comment_data)
-    # write_to_s3("pbp_data", pbp_data)
-    # write_to_s3("opp_stats", opp_stats)
+    write_to_s3("pbp_data", pbp_data)
+    write_to_s3("opp_stats", opp_stats)
     write_to_s3("twitter_tweepy_data", twitter_tweepy_data)
     # write_to_s3("schedule", schedule)
-    # write_to_s3("shooting_stats", shooting_stats)
+    write_to_s3("shooting_stats", shooting_stats)
 
     # STEP 5: Grab Logs from previous steps & send email out detailing notable events
     logs = pd.read_csv("logs/example.log", sep=r"\\t", engine="python", header=None)
