cfdff3b5f556147461074c3dd7f3c071836859a0
jacob <jyablonski9@gmail.com>
2023-06-21 18:54:14 -0500

    Ingestion Script Prod Release v1.10.1 (#65)
    * remove failed
    
    * Bump version: 1.10.0 → 1.10.1

--- src/app.py
+++ src/app.py
@@ -20,7 +20,7 @@ logging.basicConfig(
     handlers=[logging.FileHandler("logs/example.log"), logging.StreamHandler()],
 )
 logging.getLogger("requests").setLevel(logging.WARNING)  # get rid of https debug stuff
-logging.info("STARTING NBA ELT PIPELINE SCRIPT Version: 1.10.0")
+logging.info("STARTING NBA ELT PIPELINE SCRIPT Version: 1.10.1")
 
 # helper validation function - has to be here instead of utils bc of globals().items()
 def validate_schema(df: pd.DataFrame, schema: list) -> pd.DataFrame:
@@ -39,9 +39,8 @@ def validate_schema(df: pd.DataFrame, schema: list) -> pd.DataFrame:
     try:
         if (
             len(df) == 0
-        ):  # this has to be first to deal with both empty lists + valid data frames
-            logging.error(f"Schema Validation Failed for {data_name}, df is empty")
-            # df.schema = 'Invalidated'
+        ):
+            logging.error(f"df is empty for Schema Validation, skipping")
             return df
         elif list(df.columns) == schema:
             logging.info(f"Schema Validation Passed for {data_name}")
@@ -199,4 +198,4 @@ if __name__ == "__main__":
 
     # STEP 6: Send Email
     send_aws_email(logs)
-    logging.info("FINISHED NBA ELT PIPELINE SCRIPT Version: 1.10.0")
+    logging.info("FINISHED NBA ELT PIPELINE SCRIPT Version: 1.10.1")
