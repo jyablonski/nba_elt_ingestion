feefe376d7c39e394418c0d405089bc3f7571280
jacob <jyablonski9@gmail.com>
2023-08-12 13:48:49 -0500

    Ingestion Script Prod Release v1.10.2 (#66)
    - Triggering test Deploy to get Docker Image into new Account

--- src/app.py
+++ src/app.py
@@ -20,7 +20,7 @@ logging.basicConfig(
     handlers=[logging.FileHandler("logs/example.log"), logging.StreamHandler()],
 )
 logging.getLogger("requests").setLevel(logging.WARNING)  # get rid of https debug stuff
-logging.info("STARTING NBA ELT PIPELINE SCRIPT Version: 1.10.1")
+logging.info("STARTING NBA ELT PIPELINE SCRIPT Version: 1.10.2")
 
 # helper validation function - has to be here instead of utils bc of globals().items()
 def validate_schema(df: pd.DataFrame, schema: list) -> pd.DataFrame:
@@ -198,4 +198,4 @@ if __name__ == "__main__":
 
     # STEP 6: Send Email
     send_aws_email(logs)
-    logging.info("FINISHED NBA ELT PIPELINE SCRIPT Version: 1.10.1")
+    logging.info("FINISHED NBA ELT PIPELINE SCRIPT Version: 1.10.2")
