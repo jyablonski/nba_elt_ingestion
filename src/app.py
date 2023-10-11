041ba2feb08f4ef6d12d783360678bcd4d852e10
jyablonski9 <jyablonski9@gmail.com>
2023-10-11 18:59:53 -0500

    Bump version: 1.11.2 → 1.12.0

--- src/app.py
+++ src/app.py
@@ -48,7 +48,7 @@ logging.basicConfig(
     handlers=[logging.FileHandler("logs/example.log"), logging.StreamHandler()],
 )
 logging.getLogger("requests").setLevel(logging.WARNING)  # get rid of https debug stuff
-logging.info("STARTING NBA ELT PIPELINE SCRIPT Version: 1.11.2")
+logging.info("STARTING NBA ELT PIPELINE SCRIPT Version: 1.12.0")
 
 
 # helper validation function - has to be here instead of utils bc of globals().items()
@@ -226,4 +226,4 @@ if __name__ == "__main__":
 
     # STEP 6: Send Email
     send_aws_email(logs)
-    logging.info("FINISHED NBA ELT PIPELINE SCRIPT Version: 1.11.2")
+    logging.info("FINISHED NBA ELT PIPELINE SCRIPT Version: 1.12.0")
