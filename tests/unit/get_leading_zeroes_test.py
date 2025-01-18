c7a179f02a00eca148d5ee5f1fd68b3b8c1727bb
jyablonski9 <jyablonski9@gmail.com>
2025-01-18 10:25:24 -0800

    pytest mark parametrize that btch

--- tests/unit/get_leading_zeroes_test.py
+++ tests/unit/get_leading_zeroes_test.py
@@ -1,13 +1,17 @@
 from datetime import date
 
+import pytest
+
 from src.utils import get_leading_zeroes
 
 
-def test_get_leading_zeroes():
-    month_1 = get_leading_zeroes(date(2023, 1, 1).month)
-    month_9 = get_leading_zeroes(date(2023, 9, 1).month)
-    month_10 = get_leading_zeroes(date(2023, 10, 1).month)
-
-    assert month_1 == "01"
-    assert month_9 == "09"
-    assert month_10 == "10"
+@pytest.mark.parametrize(
+    "input_date, expected",
+    [
+        (date(2023, 1, 1).month, "01"),
+        (date(2023, 9, 1).month, "09"),
+        (date(2023, 10, 1).month, "10"),
+    ],
+)
+def test_get_leading_zeroes(input_date, expected):
+    assert get_leading_zeroes(value=input_date) == expected
