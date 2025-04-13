843f0a03c67a0d983ebea14efb063f68ef71625a
jyablonski9 <jyablonski9@gmail.com>
2025-04-13 08:23:59 -0700

    zz

--- src/utils.py
+++ src/utils.py
@@ -153,51 +153,6 @@ def clean_player_names(name: str) -> str:
         raise
 
 
-def write_to_s3(
-    file_name: str,
-    df: pd.DataFrame,
-    date: datetime.date = datetime.now().date(),
-    bucket: str = os.environ.get("S3_BUCKET"),
-) -> None:
-    """
-    S3 Function using awswrangler to write file.  Only supports parquet right now.
-
-    Args:
-        file_name (str): The base name of the file (boxscores, opp_stats)
-
-        df (pd.DataFrame): The Pandas DataFrame to write to S3
-
-        bucket (str): The Bucket to write to.  Defaults to `os.environ.get('S3_BUCKET')`
-
-        date (datetime.date): Date to partition the data by.
-            Defaults to `datetime.now().date()`
-
-    Returns:
-        None, but writes the Pandas DataFrame to an S3 File.
-
-    """
-    year_partition = date.year
-    month_partition = get_leading_zeroes(value=date.month)
-    file_name_jn = f"{file_name}-{date}"
-    try:
-        if len(df) == 0:
-            logging.info(f"Not storing {file_name} to s3 because it's empty.")
-            pass
-        else:
-            wr.s3.to_parquet(
-                df=df,
-                path=f"s3://{bucket}/{file_name}/validated/year={year_partition}/month={month_partition}/{file_name_jn}.parquet",
-                index=False,
-            )
-            logging.info(
-                f"Storing {len(df)} {file_name} rows to S3 (s3://{bucket}/{file_name}/validated/{year_partition}/{file_name_jn}.parquet)"
-            )
-            return None
-    except Exception as error:
-        logging.error(f"S3 Storage Function Failed {file_name}, {error}")
-        return None
-
-
 def write_to_sql(con, table_name: str, df: pd.DataFrame, table_type: str) -> None:
     """
     Simple Wrapper Function to write a Pandas DataFrame to SQL
--- tests/integration/aws_s3_test.py
+++ tests/integration/aws_s3_test.py
@@ -3,6 +3,8 @@ from datetime import datetime
 
 import boto3
 from moto import mock_s3
+import pandas as pd
+import pytest
 
 from src.aws import write_to_s3
 from src.utils import get_leading_zeroes
@@ -24,3 +26,26 @@ def test_write_to_s3_validated(player_stats_data):
         contents[0]
         == f"player_stats_data/validated/year={today.year}/month={month_prefix}/player_stats_data-{today}.parquet"  # noqa: E501s
     )
+
+
+@mock_s3
+def test_write_to_s3_empty():
+    test_file_name = "test_data"
+    bucket_name = "moto_test_bucket"
+    conn = boto3.resource("s3", region_name="us-east-1")
+    conn.create_bucket(Bucket=bucket_name)
+    df = pd.DataFrame()
+
+    # We will capture logs to check if the function correctly logs that the file is not being stored
+    with pytest.raises(Exception):
+        with pytest.capture_logs(level="INFO") as logs:
+            write_to_s3(file_name=test_file_name, df=df, bucket=bucket_name)
+
+            # Check the logs to ensure the message about the empty DataFrame is logged
+            assert "Not storing test_data to s3 because it's empty." in logs.output
+
+    # no object should have been loaded
+    bucket = conn.Bucket(bucket_name)
+    contents = [obj.key for obj in bucket.objects.all()]
+
+    assert len(contents) == 0
